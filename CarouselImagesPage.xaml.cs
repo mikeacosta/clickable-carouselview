936de02c0a9345bc84bc6ed6e6f2875a3aa64184
M Acosta <mikeacosta@zoho.com>
2017-04-28 18:57:20 -0700

    initial commit

---
+++ CarouselImagesPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace CarouselImages
+{
+	public partial class CarouselImagesPage : ContentPage
+	{
+		public CarouselImagesPage()
+		{
+			InitializeComponent();
+
+			BindingContext = new ImagesViewModel();
+		}
+	}
+}
---
+++ Image.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CarouselImages
+{
+	public class Image
+	{
+		public string Name { get; set; }
+		public string Caption { get; set; }
+
+		public Command TapCommand { get; set; }
+
+		public EventHandler TapClickEventHandler;
+
+		public Image()
+		{
+			TapCommand = new Command(() => OnImageClicked());
+		}
+
+		public void OnImageClicked()
+		{
+			TapClickEventHandler?.Invoke(this, EventArgs.Empty);
+		}
+	}
+}
